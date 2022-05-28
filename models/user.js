5756150a024a282552f5aa150f624111bc5f6d98
Igor <igr0n1wet@gmail.com>
2022-05-28 14:58:47 +0600

    finish

--- models/user.js
+++ models/user.js
@@ -36,7 +36,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: "https://chat-webka.herokuapp.com/auth/google/osekter"
+        callbackURL: "http://localhost:3000/auth/google/osekter"
     },
     function(accessToken, refreshToken, profile, cb) {
         user.findOrCreate({ googleId: profile.id }, function (err, user) {
